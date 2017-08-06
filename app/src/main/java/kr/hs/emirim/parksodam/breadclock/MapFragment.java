3a434562c63e2db31e912db03abe5154ab8dd6d8
Sodam Park <parksodam0228@e-mirim.hs.kr>
2017-08-06 16:14:55 +0900

    구글 마커 올리기 완료

--- app/src/main/java/kr/hs/emirim/parksodam/breadclock/MapFragment.java
+++ app/src/main/java/kr/hs/emirim/parksodam/breadclock/MapFragment.java
@@ -2,37 +2,56 @@ package kr.hs.emirim.parksodam.breadclock;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
  * Created by kim on 2017-07-29.
  */
-public class MapFragment extends BaseFragment{
-    private Fragment fr;
+public class MapFragment extends BaseFragment implements OnMapReadyCallback {
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
+        FrameLayout fl = (FrameLayout) view.findViewById(R.id.fl_content);
+        com.google.android.gms.maps.MapFragment fragment = new com.google.android.gms.maps.MapFragment();
+
+        SupportMapFragment map = SupportMapFragment.newInstance();
+        FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+        map.getMapAsync(this);
+        ft.add(R.id.map, map);
+        ft.commit();
+
         return view;
     }
-
-
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        FragmentManager fm = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        fragmentTransaction.replace(R.id.map, fr);
-        fragmentTransaction.commit();
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(
+                new LatLng(37.555744, 126.970431)   // 위도, 경도
+        ));
+
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+        googleMap.animateCamera(zoom);
+        MarkerOptions marker = new MarkerOptions();
+        marker .position(new LatLng(37.555744, 126.970431))
+                .title("서울역")
+                .snippet("Seoul Station");
+        googleMap.addMarker(marker).showInfoWindow(); // 마커추가,화면에출력
+
     }
+
+
     public String getTitle() {
         return "지도";
     }
