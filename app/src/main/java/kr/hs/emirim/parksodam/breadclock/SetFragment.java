1ae06c41e4e4b1e59adf63af2bc4af17d8666089
김진민 <jinmin5161@gmail.com>
2017-08-27 20:07:16 +0900

    - 설정부분 수정 - 버튼 클릭시 버튼내용 창이 뜨게끔

--- app/src/main/java/kr/hs/emirim/parksodam/breadclock/SetFragment.java
+++ app/src/main/java/kr/hs/emirim/parksodam/breadclock/SetFragment.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.parksodam.breadclock;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -10,7 +11,7 @@ import android.view.ViewGroup;
  * Created by kim on 2017-07-29.
  */
 
-public class SetFragment extends BaseFragment {
+public class SetFragment extends BaseFragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_set, container, false);
         return view;
@@ -20,7 +21,42 @@ public class SetFragment extends BaseFragment {
     public String getTitle() {
         return "설정";
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button3:
+                Intent intent = new Intent(getActivity(), NoticeActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button4:
+                Intent intent2 = new Intent(getActivity(), NoticeActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.button5:
+                Intent intent3 = new Intent(getActivity(), NoticeActivity.class);
+                startActivity(intent3);
+                break;
+            case R.id.button6:
+                Intent intent4 = new Intent(getActivity(), NoticeActivity.class);
+                startActivity(intent4);
+                break;
+            case R.id.button7:
+                Intent intent5 = new Intent(getActivity(), NoticeActivity.class);
+                startActivity(intent5);
+                break;
+            case R.id.button8:
+                Intent intent6 = new Intent(getActivity(), NoticeActivity.class);
+                startActivity(intent6);
+                break;
+            case R.id.button9:
+                Intent intent7 = new Intent(getActivity(), NoticeActivity.class);
+                startActivity(intent7);
+                break;
+        }
+    }
 }
+
 
 
 
