0beff43b3169e45456027a3322e7f2aa62c78d48
김진민 <jinmin5161@gmail.com>
2017-09-04 20:26:53 +0900

    - [즐겨찾기] 리스트뷰 띄우기 -> 누르면 새 액티비티창 뜨기 완료(알림 부분이랑 같음) - [설정] 공지사항, 자주묻는 FAQ 내용 추가(글은 완성, 디자인만 바꾸면 됨)

--- app/src/main/java/kr/hs/emirim/parksodam/breadclock/bookmark/BookmarkFragment.java
+++ app/src/main/java/kr/hs/emirim/parksodam/breadclock/bookmark/BookmarkFragment.java
@@ -1,13 +1,21 @@
 package kr.hs.emirim.parksodam.breadclock.bookmark;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import kr.hs.emirim.parksodam.breadclock.BaseFragment;
 import kr.hs.emirim.parksodam.breadclock.R;
+import kr.hs.emirim.parksodam.breadclock.listview.MyAdapter;
+import kr.hs.emirim.parksodam.breadclock.listview.MyItem;
+import kr.hs.emirim.parksodam.breadclock.notice.BreadInformation;
 
 /**
  * Created by kim on 2017-07-29.
@@ -15,10 +23,40 @@ import kr.hs.emirim.parksodam.breadclock.R;
 
 
 public class BookmarkFragment extends BaseFragment {
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        //return inflater.inflate(R.layout.fragment_bookmark, container, false);
-        View view = inflater.inflate(R.layout.fragment_bookmark, container, false);
+    private ListView mListView;
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_notice, container, false);
+
+        mListView = (ListView)view.findViewById(R.id.listView);
+        dataSetting();
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                // get item
+                MyItem item = (MyItem) parent.getItemAtPosition(position);
+                Intent intent = new Intent(getActivity(), BreadInformation.class);
+                String titleStr = item.getName();
+                String descStr = item.getContents();
+                Drawable iconDrawable = item.getIcon();
+
+                startActivity(intent);
+            }
+        }) ;
         return view;
+    }
+
+    private void dataSetting(){
+
+        MyAdapter mMyAdapter = new MyAdapter();
+
+
+        for (int i=0; i<10; i++) {
+            mMyAdapter.addItem(ContextCompat.getDrawable(getActivity(), R.mipmap.ic_launcher), "빵집 이름", "빵 이름");
+        }
+
+        /* 리스트뷰에 어댑터 등록 */
+        mListView.setAdapter(mMyAdapter);
     }
 
     @Override
---
+++ app/src/main/java/kr/hs/emirim/parksodam/breadclock/bookmark/BookmarkInformation.java
@@ -0,0 +1,19 @@
+package kr.hs.emirim.parksodam.breadclock.bookmark;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import kr.hs.emirim.parksodam.breadclock.R;
+
+/**
+ * Created by kim on 2017-09-04.
+ */
+
+public class BookmarkInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bread_information);
+    }
+}
