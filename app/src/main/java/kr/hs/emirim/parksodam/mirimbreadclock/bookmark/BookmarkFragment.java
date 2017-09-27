9a3ac52cdf1cfc376eb9da35a3d98d7eb3d3979c
Sodam Park <parksodam0228@e-mirim.hs.kr>
2017-09-28 00:04:22 +0900

    앱 최종적으로 업데이트 apk만들어서 릴리즈 키 생성받기 잊지 않기!!

--- app/src/main/java/kr/hs/emirim/parksodam/mirimbreadclock/bookmark/BookmarkFragment.java
+++ app/src/main/java/kr/hs/emirim/parksodam/mirimbreadclock/bookmark/BookmarkFragment.java
@@ -1,13 +1,16 @@
 package kr.hs.emirim.parksodam.mirimbreadclock.bookmark;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,6 +19,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.iamhabib.easy_preference.EasyPreference;
 
 import java.util.ArrayList;
 
@@ -25,6 +30,7 @@ import kr.hs.emirim.parksodam.mirimbreadclock.BaseFragment;
 import kr.hs.emirim.parksodam.mirimbreadclock.LoginActivity;
 import kr.hs.emirim.parksodam.mirimbreadclock.R;
 import kr.hs.emirim.parksodam.mirimbreadclock.model.BookmarkBakery;
+import noman.googleplaces.Place;
 
 
 /**
@@ -38,8 +44,10 @@ public class BookmarkFragment extends BaseFragment {
     private String TAG ="where is uid??";
     private FirebaseAuth.AuthStateListener mAuthListener;
     private String bakery;
+    private static String name;
+    private static String location;
 
-    ArrayList<BookmarkBakery> mBookmarkBakeries = new ArrayList<>();
+    ArrayList<BookmarkBakery> seachedBakeris = new ArrayList<>();
     MyAdapter mMyAdapter;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -66,27 +74,66 @@ public class BookmarkFragment extends BaseFragment {
         };
 
         mListView = (ListView)view.findViewById(R.id.listView);
+        final Place place = new Place();
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("즐겨찾기");
+                builder.setMessage("즐겨찾기를 삭제하시겠습니까?" );
+                builder.setCancelable(true);
+                builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
 
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        BookmarkBakery bb = seachedBakeris.get(position);
+                        DatabaseReference bookmarkRef = ((BarActivity)getActivity()).mDatabase.getReference("users/"+ mAuth.getCurrentUser().getUid()+"/bookmarks/" + bb.uid);
+                        Log.e(TAG, "빵집 삭제 : " + bb.uid);
+                        bookmarkRef.setValue(null);
+
+                        FirebaseMessaging.getInstance().subscribeToTopic(bb.uid);
+                        //Log.e(TAG,bb.uid);
+                        EasyPreference.with(getActivity())
+                                .addString(name,place.getName())
+                                .save();
+
+                        EasyPreference.with(getActivity())
+                                .addString(location,place.getVicinity())
+                                .save();
+
+                        Log.d(TAG,"이름 저장");
+                        Log.d(TAG,"위치 저장");
+                    }
+                });
+                builder.setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id){
+                        dialog.cancel();
+                    }
+                });
+                builder.create().show();
+            }
+        });
         DatabaseReference userBookmarkRef = ((BarActivity) getActivity()).mDatabase.getReference("users/"+ mAuth.getCurrentUser().getUid()+"/bookmarks");
         ValueEventListener userBookmarkListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+@Override
+public void onDataChange(DataSnapshot dataSnapshot) {
 
-                mBookmarkBakeries.clear();
-                for(DataSnapshot bookmarkBakerySnapshot:dataSnapshot.getChildren()){
-                    BookmarkBakery bb =  bookmarkBakerySnapshot.getValue(BookmarkBakery.class);
-                    mBookmarkBakeries.add(bb);
-                    Log.e(TAG, "내가 좋아하는 빵집은요 : " + bb.name );
-                }
-                mMyAdapter = new MyAdapter(mBookmarkBakeries);
-                mListView.setAdapter(mMyAdapter);
-                //mListView.invalidate();
-            }
+    seachedBakeris.clear();
+        for(DataSnapshot bookmarkBakerySnapshot:dataSnapshot.getChildren()){
+        BookmarkBakery bb =  bookmarkBakerySnapshot.getValue(BookmarkBakery.class);
+            seachedBakeris.add(bb);
+        Log.e(TAG, "내가 좋아하는 빵집은요 : " + bb.name );
+        }
+        mMyAdapter = new MyAdapter(seachedBakeris);
+        mListView.setAdapter(mMyAdapter);
+        //mListView.invalidate();
+        }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+@Override
+public void onCancelled(DatabaseError databaseError) {
 
-            }
+        }
         };
         userBookmarkRef.addValueEventListener(userBookmarkListener);
 
@@ -106,35 +153,35 @@ public class BookmarkFragment extends BaseFragment {
 //            }
 //        }) ;
         return view;
-    }
-    @Override
-    public void onStart() {
+        }
+@Override
+public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         updateUI(currentUser);
-    }
-    @Override
-    public void onStop() {
+        }
+@Override
+public void onStop() {
         super.onStop();
         if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
+        mAuth.removeAuthStateListener(mAuthListener);
         }
-    }
-    private void updateUI(FirebaseUser user) {
+        }
+private void updateUI(FirebaseUser user) {
         if (user != null) {
-            Log.e(TAG,"현재 uid : "+user.getUid());
-            mAuth = FirebaseAuth.getInstance();
+        Log.e(TAG,"현재 uid : "+user.getUid());
+        mAuth = FirebaseAuth.getInstance();
 //            FirebaseDatabase database = FirebaseDatabase.getInstance();
 //            bakery = "베이크팡";
 //            DatabaseReference myRef = database.getReference("BreadClockWeb/Bakeries/BasicInfo/"+bakery+"/Favorites/"+user.getUid());
 //            myRef.setValue(user.getEmail());
         } else {
-            Intent intent = new Intent(getActivity(),LoginActivity.class);
-            startActivity(intent);
+        Intent intent = new Intent(getActivity(),LoginActivity.class);
+        startActivity(intent);
         }
-    }
-    private void dataSetting() {
+        }
+private void dataSetting() {
 
 
 
@@ -147,11 +194,10 @@ public class BookmarkFragment extends BaseFragment {
         /* 리스트뷰에 어댑터 등록 */
         mListView.setAdapter(mMyAdapter);
 
-    }
+        }
 
 
-
-    @Override
+@Override
     public String getTitle() {
         return "즐겨찾기";
     }
--- app/src/main/java/kr/hs/emirim/parksodam/mirimbreadclock/notice/NoticeFragment.java
+++ app/src/main/java/kr/hs/emirim/parksodam/mirimbreadclock/notice/NoticeFragment.java
@@ -1,21 +1,30 @@
 package kr.hs.emirim.parksodam.mirimbreadclock.notice;
 
-import android.content.IntentFilter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-import kr.hs.emirim.parksodam.mirimbreadclock.Adapter.AlarmsAdapter;
+import kr.hs.emirim.parksodam.mirimbreadclock.Adapter.MyAdapter;
+import kr.hs.emirim.parksodam.mirimbreadclock.BarActivity;
 import kr.hs.emirim.parksodam.mirimbreadclock.BaseFragment;
+import kr.hs.emirim.parksodam.mirimbreadclock.LoginActivity;
 import kr.hs.emirim.parksodam.mirimbreadclock.R;
-import kr.hs.emirim.parksodam.mirimbreadclock.model.Alarm;
+import kr.hs.emirim.parksodam.mirimbreadclock.model.BookmarkBakery;
 
 
 /**
@@ -23,47 +32,122 @@ import kr.hs.emirim.parksodam.mirimbreadclock.model.Alarm;
  */
 
 
-public class NoticeFragment extends BaseFragment implements LoadAlarmsReceiver.OnAlarmsLoadedListener {
-    private LoadAlarmsReceiver mReceiver;
-    private AlarmsAdapter mAdapter;
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_notice, container, false);
-        final EmptyRecyclerView rv = (EmptyRecyclerView) view.findViewById(R.id.recycler);
-        mAdapter = new AlarmsAdapter();
-        rv.setEmptyView(view.findViewById(R.id.empty_view));
-        rv.setAdapter(mAdapter);
-        rv.addItemDecoration(new DividerItemDecoration(getContext()));
-        rv.setLayoutManager(new LinearLayoutManager(getContext()));
-        rv.setItemAnimator(new DefaultItemAnimator());
+public class NoticeFragment extends BaseFragment {
+    private ListView mListView;
+    private FirebaseAuth mAuth;
+    private String TAG ="where is uid??";
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private String bakery;
 
+    ArrayList<BookmarkBakery> seachedBakeris = new ArrayList<>();
+    MyAdapter mMyAdapter;
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        View view = inflater.inflate(R.layout.fragment_notice, container, false);
+        Log.e(TAG,"성공");
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                // Write a message to the database
+                if (mAuth != null) {
+
+
+                } else {
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    startActivity(intent);
+                }
+                if (mAuthListener != null) {
+                    mAuth.removeAuthStateListener(mAuthListener);
+                }
+            }
+        };
+
+        mListView = (ListView)view.findViewById(R.id.listView);
+
+        DatabaseReference userBookmarkRef = ((BarActivity) getActivity()).mDatabase.getReference("users/"+ mAuth.getCurrentUser().getUid()+"/bookmarks");
+        ValueEventListener userBookmarkListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                seachedBakeris.clear();
+                for(DataSnapshot bookmarkBakerySnapshot:dataSnapshot.getChildren()){
+                    BookmarkBakery bb =  bookmarkBakerySnapshot.getValue(BookmarkBakery.class);
+                    seachedBakeris.add(bb);
+                    Log.e(TAG, "내가 좋아하는 빵집은요 : " + bb.name );
+                }
+                mMyAdapter = new MyAdapter(seachedBakeris);
+                mListView.setAdapter(mMyAdapter);
+                //mListView.invalidate();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        userBookmarkRef.addValueEventListener(userBookmarkListener);
+
+        // dataSetting();
+//
+//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView parent, View v, int position, long id) {
+//                // get item
+//                MyItem item = (MyItem) parent.getItemAtPosition(position);
+//                Intent intent = new Intent(getActivity(), BookmarkInformation.class);
+//                String titleStr = item.getName();
+//                String descStr = item.getContents();
+//                Drawable iconDrawable = item.getIcon();
+//
+//                startActivity(intent);
+//            }
+//        }) ;
         return view;
     }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mReceiver = new LoadAlarmsReceiver(this);
-    }
-
     @Override
     public void onStart() {
         super.onStart();
-        final IntentFilter filter = new IntentFilter(LoadAlarmsService.ACTION_COMPLETE);
-        LocalBroadcastManager.getInstance(getContext()).registerReceiver(mReceiver, filter);
-        LoadAlarmsService.launchLoadAlarmsService(getContext());
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
-
     @Override
     public void onStop() {
         super.onStop();
-        LocalBroadcastManager.getInstance(getContext()).unregisterReceiver(mReceiver);
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Log.e(TAG,"현재 uid : "+user.getUid());
+            mAuth = FirebaseAuth.getInstance();
+//            FirebaseDatabase database = FirebaseDatabase.getInstance();
+//            bakery = "베이크팡";
+//            DatabaseReference myRef = database.getReference("BreadClockWeb/Bakeries/BasicInfo/"+bakery+"/Favorites/"+user.getUid());
+//            myRef.setValue(user.getEmail());
+        } else {
+            Intent intent = new Intent(getActivity(),LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+    private void dataSetting() {
 
-    @Override
-    public void onAlarmsLoaded(ArrayList<Alarm> alarms) {
-        mAdapter.setAlarms(alarms);
+
+
+
+//        for (int i = 0; i < 8; i++) {
+//            mMyAdapter.addItem(ContextCompat.getDrawable(getActivity(), R.mipmap.basicimg), "빵집 이름", "빵 이름", ContextCompat.getDrawable(getActivity(), R.drawable.star_select));
+//        }
+
+
+        /* 리스트뷰에 어댑터 등록 */
+        mListView.setAdapter(mMyAdapter);
+
     }
-    @Override
     public String getTitle() {
         return "알람";
     }
